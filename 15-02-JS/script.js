b78933dc37cd04cda661d5e0e3a0ac1da420dc1c
Inese Krole <inese.krole@gmail.com>
2022-02-17 17:55:07 +0200

    15.02.

---
+++ 15-02-JS/script.js
@@ -0,0 +1,42 @@
+// let a = 1;
+// let b = 2121.3232;
+// let bigInt = 2222222222222222222222222n;
+// let specialNumber = 5E-1;
+// console.log(specialNumber);
+// a = 2;
+// b = 1;
+// console.log(a + b);
+// d = 0.2;
+// e = 0.1;
+// console.log(d + e);
+// console.log((d + e) == 0.3);
+// //0.1 +0.2 - 0.3 ==0 gives false!
+
+// let myNumber = Number("6434343");//conversion!!
+// console.log(myNumber);
+// console.log(typeof (myNumber));
+
+myNumber2 = parseInt("7645685");
+console.log(myNumber2);
+
+myNumber = Number("434343,3232".replace(",", "."));
+myNumber = Number("5656565ggggg");
+console.log(myNumber);//gives NaN
+
+//base ^
+
+
+//boolean
+let answer = true;
+console.log(typeof (answer));
+
+console.log("1" + "1");
+
+console.log(Number("0") === 0);
+
+
+console.log("aa" < "b");
+
+a = 4;
+console.log(a ** 2);
+// console.log(++a);
