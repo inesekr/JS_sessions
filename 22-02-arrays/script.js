492687b0d5de2a8f943ef889a257e033bf824f7f
Inese Krole <inese.krole@gmail.com>
2022-02-22 21:25:28 +0200

    fizzBuzz

---
+++ 22-02-arrays/script.js
@@ -0,0 +1,18 @@
+let numberOfFizzBuzz = parseInt(prompt("Enter the number of Fizz-Buzzes"));
+
+let i = 1;
+let fizzBuzz = 0;
+
+do {
+
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        fizzBuzz++;
+        console.log(i);
+        i++;
+
+    }
+
+    else { i++ };
+
+} while (fizzBuzz < numberOfFizzBuzz)
