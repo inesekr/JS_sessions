0984a94ebb1d016630aa4494755380d230a17b56
Inese Krole <inese.krole@gmail.com>
2022-02-27 19:05:53 +0200

    24.02. functions

--- 22-02-arrays/script.js
+++ 22-02-arrays/script.js
@@ -4,15 +4,10 @@ let i = 1;
 let fizzBuzz = 0;
 
 do {
-
-
     if (i % 3 == 0 && i % 5 == 0) {
         fizzBuzz++;
         console.log(i);
         i++;
-
     }
-
     else { i++ };
-
-} while (fizzBuzz < numberOfFizzBuzz)
+} while (fizzBuzz < numberOfFizzBuzz);
---
+++ 24-02-objects-functions/script.js
@@ -0,0 +1,54 @@
+// let numberOfFizzBuzz = parseInt(prompt("Enter the number of Fizz-Buzzes"));
+
+// let current = 1;
+// let sk = 0;
+// while (sk < numberOfFizzBuzz) {
+//     let previousStr;
+//     let previousNumber;
+//     else if (current % 3 == 0) {
+//         previousStr = "Fizz";
+//     }
+//     else if (current % 5 == 0) {
+//         if (previousStr === "Fizz") {
+//             console.log(current);
+//             previousStr = "Fizz";
+//             previousNumber = current;
+//         }
+//     }
+//     current++;
+// }
+
+
+
+// function getSum(a, b) {
+//     // let c = a+b;
+//     // return c;
+
+//     //or this way:
+//     return a + b;
+// }
+
+// console.log(getSum(3, 8));
+
+// function sum() {
+//     return arguments[0] + arguments[1];
+// }
+
+// mySum(3, 9);
+// function mySum(a, b) {
+//     console.log(a + b)
+// }
+
+// alert(mySum);
+
+// 5! = 5*4*3*2*1 // factorial
+
+// function fact(n) {
+//     if (n == 1) {
+//         return 1
+//     }
+//     else {
+//         return n * fact(n - 1);
+//     }
+// }
+
