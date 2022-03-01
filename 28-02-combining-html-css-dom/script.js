f145b1b5f8291ac202ed77bb4e6c762bd92e5a34
Inese Krole <inese.krole@gmail.com>
2022-03-01 18:36:39 +0200

    28.02.

---
+++ 28-02-combining-html-css-dom/script.js
@@ -0,0 +1,8 @@
+// console.log(title);
+
+// document.title= "Changed title"
+
+// this.newElement
+console.log(this.document.getElementById("MyPara"));
+console.log(document.getElementsByTagName("p"));
+console.log(document.getElementsByName(MyTextName));
