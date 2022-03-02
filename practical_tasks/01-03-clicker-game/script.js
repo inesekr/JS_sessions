9e84ab68425c4be21d49cf335998dae6a7f3d3a8
Inese Krole <inese.krole@gmail.com>
2022-03-02 12:31:12 +0200

    clicker game improved

--- practical_tasks/01-03-clicker-game/script.js
+++ practical_tasks/01-03-clicker-game/script.js
@@ -1,14 +1,19 @@
 let counter = 0;
+
 function clicker() {
 
     counter++;
+    if (counter === 1) {//timer start on first click!
+
+        const gameTime = setTimeout(game, 7000);
+        function game() {
+            alert(`During 7 seconds you clicked ${counter} times`);
+            counter = 0;
+            counterLine.textContent = "Here you will see your result";
+        }
+    }
 
     let counterLine = document.getElementById("result");
     counterLine.textContent = `${counter} clicks so far`;
 }
-
-
-setTimeout(function () {
-    alert(`During 10 seconds you clicked ${counter} times`)
-}, 10000);
 
