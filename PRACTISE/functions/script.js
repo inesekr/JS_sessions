76c98477c7089e8d12d6a493c7eecb34c44ebda2
Inese Krole <inese.krole@gmail.com>
2022-03-03 18:21:04 +0200

    few more ideas on functions task

--- PRACTISE/functions/script.js
+++ PRACTISE/functions/script.js
@@ -23,29 +23,90 @@ var myString = inputText.toString();
 function palindrome(myString) {
     var re = /[^A-Za-z0-9]/g;
     myString = myString.toLowerCase().replace(re, '');
-    var len = myString.length;
-    for (var i = 0; i < len / 2; i++) {
-        if (myString[i] === myString[len - 1 - i]) {
-            console.log(`Your text "${myString}" is palindrome`) //prints to console each time when a match is found...
+    // var len = myString.length;
+
+    var myArray = Array.from(myString);
+    console.log(myArray);
+    var len = myArray.length;
+    // for (var i = 0; i < len / 2; i++) {
+    var isPalindrome = myArray.every(function () {
+        for (var i = 0; i < len / 2; i++) {
+            myArray[i] === myArray[len - 1 - i]
         }
-        else {
-            console.log(`Your text "${myString}" is NOT palindrome`)
-        }
+    })
+    if (isPalindrome === true) {
+        console.log(`Your text "${myString}" is palindrome`)
+    }
+    else {
+        console.log(`Your text "${myString}" is NOT palindrome`);
     }
 }
 
+
+
+
+//     if (isPalindrome === true) {
+//         console.log(`Your text "${myString}" is palindrome`)
+//     }
+
+//     else { console.log(`Your text "${myString}" is NOT palindrome`); }
+// }
+
+// for (var i = 0; i < len / 2; i++) {
+
+// let numbers = [1, 3, 5];
+// let result = numbers.every(function (e) {
+//     return e > 0;
+// });
+
+// var isPalindrome = myString.every(function () {
+//     myString[i] === myString[len - 1 - i]
+// });
+// if (isPalindrome === true) {
+//     console.log(`Your text "${myString}" is palindrome`)
+// }
+
+// else { console.log(`Your text "${myString}" is NOT palindrome`); }
+
+
+
+
+
+
+
+// if (myString[i] !== myString[len - 1 - i]) {
+//     console.log(`Your text "${myString}" is NOT palindrome`);
+//     break;
+// }
+// else {
+//     console.log(`Your text "${myString}" is palindrome`)
+// } //if put here, it prints to console each time when a match is found...
+
+
 palindrome(myString);
+
+
+// myString = myString.toLowerCase().replace(re, '');
+// var myArray = Array.from(myString);
+
+// function makeAnagram() {
+
+//     var makeAnagram = myArray[Math.floor(Math.random() * myArray.length)];
+//     console.log(`An anagram from your `)
+// }
+// makeAnagram();
+
 
 // anagram(myString);??
 
-function anagram(myString, words) {
-    var re = /[^A-Za-z0-9]/g;
-    myString = myString.toLowerCase().replace(re, '');
-    var a = myString.split('').sort().join('');
-    var b = words.join('').split('').sort().join('');
-    return a == b;
-}
-anagram(myString);
+// function anagram(myString, words) {
+//     var re = /[^A-Za-z0-9]/g;
+//     myString = myString.toLowerCase().replace(re, '');
+//     var a = myString.split('').sort().join('');
+//     var b = words.join('').split('').sort().join('');
+//     return a == b;
+// }
+// anagram(myString);
 
 
 function wordCount(myString) {
