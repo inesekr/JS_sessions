93bcc02fdee7169bd77d03e927183aeb8ec969ba
Inese Krole <inese.krole@gmail.com>
2022-03-06 16:31:47 +0200

    functions practise final version

--- PRACTISE/functions/script.js
+++ PRACTISE/functions/script.js
@@ -1,7 +1,9 @@
 var inputText = window.prompt("Please put in some text here!");
 var myString = inputText.toString();
 
-// function palindrome(myString) {
+// Here is version with function, without loop:
+
+// function palindrome(myString) { 
 //     // Lowercase the string and use the RegExp to remove unwanted characters from it
 //     var re = /[\W_]/g; // or var re = /[^A-Za-z0-9]/g;
 
@@ -19,94 +21,49 @@ var myString = inputText.toString();
 //     }
 // }
 
-
 function palindrome(myString) {
     var re = /[^A-Za-z0-9]/g;
     myString = myString.toLowerCase().replace(re, '');
-    // var len = myString.length;
+    var len = myString.length;
 
-    var myArray = Array.from(myString);
-    console.log(myArray);
-    var len = myArray.length;
-    // for (var i = 0; i < len / 2; i++) {
-    var isPalindrome = myArray.every(function () {
-        for (var i = 0; i < len / 2; i++) {
-            myArray[i] === myArray[len - 1 - i]
+    for (var i = 0; i < len / 2; i++) {
+
+        if (myString[i] !== myString[len - 1 - i]) {
+            console.log(`Your text "${myString}" is NOT palindrome`);
+            break;
         }
-    })
-    if (isPalindrome === true) {
-        console.log(`Your text "${myString}" is palindrome`)
-    }
-    else {
-        console.log(`Your text "${myString}" is NOT palindrome`);
+        else {
+            console.log(`Your text "${myString}" is palindrome`)
+        } // unfortunately, it prints to console each time when a match is found...
     }
 }
-
-
-
-
-//     if (isPalindrome === true) {
-//         console.log(`Your text "${myString}" is palindrome`)
-//     }
-
-//     else { console.log(`Your text "${myString}" is NOT palindrome`); }
-// }
-
-// for (var i = 0; i < len / 2; i++) {
-
-// let numbers = [1, 3, 5];
-// let result = numbers.every(function (e) {
-//     return e > 0;
-// });
-
-// var isPalindrome = myString.every(function () {
-//     myString[i] === myString[len - 1 - i]
-// });
-// if (isPalindrome === true) {
-//     console.log(`Your text "${myString}" is palindrome`)
-// }
-
-// else { console.log(`Your text "${myString}" is NOT palindrome`); }
-
-
-
-
-
-
-
-// if (myString[i] !== myString[len - 1 - i]) {
-//     console.log(`Your text "${myString}" is NOT palindrome`);
-//     break;
-// }
-// else {
-//     console.log(`Your text "${myString}" is palindrome`)
-// } //if put here, it prints to console each time when a match is found...
-
-
 palindrome(myString);
 
 
-// myString = myString.toLowerCase().replace(re, '');
-// var myArray = Array.from(myString);
 
-// function makeAnagram() {
+function makeAnagram() {
+    var re = /[^A-Za-z0-9]/g;
+    myString = myString.toLowerCase().replace(re, '');
+    var myArray = Array.from(myString);
+    // console.log(myArray);
+    let myNewArray = [];
 
-//     var makeAnagram = myArray[Math.floor(Math.random() * myArray.length)];
-//     console.log(`An anagram from your `)
-// }
-// makeAnagram();
+    for (var i = myArray.length - 1; i >= 0; i--) {
 
+        var randomElement = myArray[Math.floor(Math.random() * myArray.length)];
+        // console.log(randomElement);
+        var indexToRemove = myArray.indexOf(randomElement);
+        // console.log(indexToRemove);
+        myArray.splice(indexToRemove, 1);
+        // console.log(myArray);
 
-// anagram(myString);??
-
-// function anagram(myString, words) {
-//     var re = /[^A-Za-z0-9]/g;
-//     myString = myString.toLowerCase().replace(re, '');
-//     var a = myString.split('').sort().join('');
-//     var b = words.join('').split('').sort().join('');
-//     return a == b;
-// }
-// anagram(myString);
+        myNewArray.push(randomElement);
+        var anagram = myNewArray.join("");
+        // or this way: var anagram = myNewArray.toString().replace(re, '');
+    }
+    console.log(`One anagram of your original string "${myString}" is "${anagram}"`);
+}
+makeAnagram();
 
 
 function wordCount(myString) {
@@ -117,12 +74,10 @@ function wordCount(myString) {
         }
         else {
             totalSoFar++; // add 1 to total so far
-
         }
     }
     console.log(`Your string "${myString}" consists of ${totalSoFar} words`);
 }
-
 wordCount(myString);
 // unfortunately this doesn't solve the case if 2 spaces are used together... :(
 
@@ -137,7 +92,6 @@ function getMaxChar(myString) {
         }
     });
     console.log(`Max occuring character in your string "${myString}" is ${maxChar} and it occurs ${max - 1} times`)
-    // return maxChar;
 };
 getMaxChar(myString);
 // if 2 or more characters are of the same count - it takes one (first it finds?)
