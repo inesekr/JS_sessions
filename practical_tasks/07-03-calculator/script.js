847ef648b9147e6d55c20e5e857febefdb1b2e83
Inese Krole <inese.krole@gmail.com>
2022-03-10 18:29:17 +0200

    jQuery calculator done

--- practical_tasks/07-03-calculator/script.js
+++ practical_tasks/07-03-calculator/script.js
@@ -1,123 +1,174 @@
+
 $(document).ready(function () {
 
-    let firstNum = '';
+    let firstNum = '0';
     let secondNum = '';
     let operator = '';
     let result = '';
     let output = $(".screen").html();
+    // console.log(output);
 
-    function calculate(){
+    function calculate() {
 
-        if (operator === "+"){
+        if (operator === "+") {
             result = Number(firstNum) + Number(secondNum)
         }
-                        
-        else if(operator=== "-"){
-            result = firstNum-secondNum;
+
+        else if (operator === "-") {
+            result = firstNum - secondNum;
         }
-    
-        else if(operator=== "*"){
+
+        else if (operator === "*") {
             result = firstNum * secondNum; //with decimal numbers gives incorrect result - e.g. "0.8*3=2.4000000000000004"
         }
-    
-        else if(operator=== "/"){
-            result = firstNum/secondNum;
+
+        else if (operator === "/") {
+            if (secondNum === "0") {
+                result = firstNum / secondNum;
+                alert("You can not divide by zero! Please press AC and try anything else...")
+            }
+            else {
+                result = firstNum / secondNum;
+            }
         }
-        
     }
 
     $("button").on("click", function () {
 
         let buttonClicked = $(this).html();
-     
-        if (buttonClicked >= '0' && buttonClicked <= '9' || buttonClicked === '.') {
 
-            if(operator=== ''){
-                firstNum = firstNum + buttonClicked;
-                // console.log(firstNum);
+        if (buttonClicked >= '0' && buttonClicked <= '9') {
+
+            if (operator === '' && firstNum !== result) {
+                if (firstNum === "0") {
+                    firstNum = buttonClicked;
+                    output = $(".screen").text(buttonClicked);
+                }
+                else {
+                    firstNum = firstNum + buttonClicked;
+                    output = $(".screen").text($(".screen").html() + buttonClicked)
+                }
+            }
+
+            else if (operator === '' && firstNum === result) {
+                alert("You can't modify the result!")
+                firstNum = result;
+                $(".screen").text($(".screen").html());
+            }
+            else if (operator !== '') {
+                secondNum = secondNum + buttonClicked;
                 output = $(".screen").text($(".screen").html() + buttonClicked);
             }
-            else{
-                secondNum = secondNum+ buttonClicked;
-                // console.log(secondNum);
-                output = $(".screen").text($(".screen").html() + buttonClicked);
-            }           
         }
+
+
+        else if (buttonClicked === '.') {
+
+            if (operator === '') {
+
+                if (firstNum === result) {
+                    alert("sorry, you can't modify the result");
+                    firstNum = result;
+                    $(".screen").text($(".screen").html())
+                }
+
+                else {
+                    if (firstNum.includes('.')) {
+                        firstNum = firstNum;
+                        alert("You can't use dot now");
+                        output = $(".screen").text($(".screen").html());
+                    }
+                    else {
+                        firstNum = firstNum + buttonClicked;
+                        output = $(".screen").text($(".screen").html() + buttonClicked);
+                    }
+                }
+            }
+
+            else {
+
+                if (secondNum.includes('.')) {
+                    secondNum = secondNum;
+                    alert("You can't use dot now");
+                    output = $(".screen").text($(".screen").html());
+                }
+                else {
+                    secondNum = secondNum + buttonClicked;
+                    output = $(".screen").text($(".screen").html() + buttonClicked);
+                }
+            }
+        }
+
 
         else if (buttonClicked === '+' || buttonClicked === '-' || buttonClicked === '*' || buttonClicked === '/') {
 
             if (firstNum === '') {
-                $(".screen").text();
+                $(".screen").html("0");
             }
 
-            else if ( firstNum !== '' && operator===''){
+            else if (firstNum !== '' && operator === '') {
                 operator = buttonClicked;
                 output = $(".screen").text($(".screen").html() + buttonClicked);
-
             }
 
-            else if(operator!== '' && secondNum !== "") {
-               calculate();
-               output = $(".screen").text($(".screen").html() + "=" + result + buttonClicked);  
-                // output = $(".screen").text($(".screen").html() + result);  
-        firstNum = result;
-        secondNum = '';
-        operator = buttonClicked;
-            } 
+            else if (operator !== '' && secondNum !== "") {
+                calculate();
+                output = $(".screen").text($(".screen").html() + "=" + result + buttonClicked);
+
+                firstNum = result;
+                secondNum = '';
+                operator = buttonClicked;
+            }
         }
+
 
         else if (buttonClicked === 'DEL') {
 
-            // function deleteCharacter() {
-            //     let currentValue = $('.inputDisplay').val();
-            //     $('.inputDisplay').val(currentValue.substring(0, currentValue.length - 1));
-            // }
+            if (operator === '' && firstNum !== '' && firstNum !== result) {
+                firstNum = firstNum.substring(0, firstNum.length - 1);
+                $(".screen").text($(".screen").html().substring(0, ($(".screen").html().length) - 1));
+            }
 
+            else if (operator === '' && firstNum === result) {
+                alert("sorry, you can't modify the result");
+                firstNum = result;
+                $(".screen").text($(".screen").html());
+            }
 
-            // output = $(".screen").text($(".screen").html().substring(0, $(".screen").text($(".screen").html().length) - 1))
+            else if (secondNum !== '') {
+                secondNum = secondNum.substring(0, secondNum.length - 1);
+                $(".screen").text($(".screen").html().substring(0, ($(".screen").html().length) - 1));
+            }
 
-            output = $(".screen").html().length - 1;
-
-            // output = (output.length) - 1
+            else if (secondNum === '' && operator !== '') {
+                operator = '';
+                $(".screen").text($(".screen").html().substring(0, ($(".screen").html().length) - 1));
+            }
+            else if (operator === '' && firstNum === '') {
+                $(".screen").html("0");
+            }
 
         }
 
         else if (buttonClicked === 'AC') {
             firstNum = '';
             secondNum = '';
-            operator ='';
-            // output = '';
-            $(".screen").html("");// this works OK!!
+            operator = '';
+            $(".screen").html("0");// this works OK!!
         }
 
-else if (buttonClicked === '='){
-    if(firstNum !=="" && secondNum !== "" && operator !== ""){
-        
-        output = $(".screen").text($(".screen").html() + "=");
+        else if (buttonClicked === '=') {
+            if (firstNum !== "" && secondNum !== "" && operator !== "") {
 
-calculate();
+                output = $(".screen").text($(".screen").html() + "=");
 
-output = $(".screen").text($(".screen").html() + result);  
-        firstNum = result;
-        secondNum = '';
-        operator = ''
+                calculate(); //calculating is correct for numbers of up to 16 digits including.
 
+                output = $(".screen").text($(".screen").html() + result);
+                firstNum = result;
+                secondNum = '';
+                operator = ''
             }
         }
     })
 })
-
-
-    
-
-    //     function handleTotal() { switch (operator) { case '+': total = +num1 + +num2; $(".screen").text(total); break; } }
-    
-      
-    // isNumber = function (value) {
-    //     return !isNaN(value);
-    // }
-    // updateScreen = function (displayValue) {
-    //     var displayValue = displayValue.toString();
-    //     $('.first-number').html(displayValue.substring(0, 10));
-    // };
-
